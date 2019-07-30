5a7e0696ceb35330000ae7617d1cd1a9378e6379
James Scalise <jamescscalise@gmail.com>
2019-07-30 19:40:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,44 @@
+def using_push(array, string)
+  return array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array, num = 2)
+  return array.pop(num)
+end
+
+def shift_with_args(array, num = 2)
+  return array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+  return array1
+end
+
+def using_insert(array, elment)
+  array.insert(4, element)
+  return array
+end
+
+def using_uniq
+  array.usiq
+  return array
+end
+
+def using_flatten
+  return array.flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
